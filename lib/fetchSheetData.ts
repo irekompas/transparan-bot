cc10226c5db1c9d5b93c46c3f5dffe4f0473e49d
irekompas <ire.sarwindaningrum@gmail.com>
2026-04-20 10:24:24 +0700

    Update fetchSheetData.ts

--- lib/fetchSheetData.ts
+++ lib/fetchSheetData.ts
@@ -18,8 +18,7 @@ export interface NewsItem {
 }
 
 const SHEET_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSwDmKQ55VvM_BJqSdISJbERkHa23JBe0ER_c5mneaA5AOs5hqSQt0QgfHJ49qEmAj4ianyAik-TOJ4/pub?output=tsv
-";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSwDmKQ55VvM_BJqSdISJbERkHa23JBe0ER_c5mneaA5AOs5hqSQt0QgfHJ49qEmAj4ianyAik-TOJ4/pub?output=tsv";
 
 let cachedData: NewsItem[] | null = null;
 let cacheTimestamp = 0;
